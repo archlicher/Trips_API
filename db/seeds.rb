14ca8d6153a4d4b37b9ea0424354578529fce1ec
archlicher <hristofor_todorov@barinsports.com>
2026-03-23 09:23:34 +0200

    [Trip-API-2] Added the Seed.rb and the data to be seeded.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# db/seeds.rb
+require 'json'
+
+file = File.read(Rails.root.join('db/data.json'))
+data = JSON.parse(file)
+
+data.each do |trip|
+  Trip.create!(
+    name: trip["name"],
+    image_url: trip["image"],
+    short_description: trip["description"],
+    long_description: trip["long_description"],
+    rating: trip["rating"]
+  )
+end
