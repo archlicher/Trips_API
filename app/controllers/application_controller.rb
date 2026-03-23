87347ba175f39eebea451ea8a3d8ac67470492f8
Hristofor Todorov <christophergbbg@yahoo.com>
2026-03-23 18:00:47 +0200

    [Trip-API-6] Error handling.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+	# app/controllers/application_controller.rb
+  rescue_from ActiveRecord::RecordNotFound do
+    render json: { error: "Not Found" }, status: :not_found
+  end
 end
